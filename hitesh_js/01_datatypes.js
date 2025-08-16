5aa24393c39f4f5d072e5104df38339179e75cfd
SHASHI BHOOSHAN RAJ <132089716+Shashi7667@users.noreply.github.com>
2025-08-17 00:40:27 +0530

    Create 01_datatypes.js
    fdgfdgdfs

---
+++ hitesh_js/01_datatypes.js
@@ -0,0 +1,20 @@
+
+console.log("Hello, World!"); // Output: Hello, World!
+
+let name = "Shashi";
+let lname = "Raj"
+console.log("My name is", name, lname); 
+
+//number =>? 2to power 52
+// bigint
+//string => " "
+// boolean => true / false
+// null =>standalone value
+// undefined => no value assigned
+// symbol => unique value
+
+
+
+//objects
+
+
