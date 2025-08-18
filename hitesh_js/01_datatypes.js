42746dbb41fa9354df7c02518ba14d47afa8a026
SHASHI BHOOSHAN RAJ <132089716+Shashi7667@users.noreply.github.com>
2025-08-19 01:40:52 +0530

    Update 01_datatypes.js
    uplode

--- hitesh_js/01_datatypes.js
+++ hitesh_js/01_datatypes.js
@@ -1,9 +1,9 @@
 
-console.log("Hello, World!"); // Output: Hello, World!
+// console.log("Hello, World!"); // Output: Hello, World!
 
-let name = "Shashi";
-let lname = "Raj"
-console.log("My name is", name, lname); 
+// let name = "Shashi";
+// let lname = "Raj"
+// console.log("My name is", name, lname); 
 
 //number =>? 2to power 52
 // bigint
@@ -17,4 +17,22 @@ console.log("My name is", name, lname);
 
 //objects
 
+// let id=Symbol("Shashi");
+// let id2=Symbol("Shashi1");
+// console.log(id === id2);  // false , because symbol are unique
+
+
+// let users={
+//     name:"Shashi",
+//     age:23
+// };
+// console.log(`my name is ${users.name} and my age is ${users.age}`);
+
+
+// let a =10;
+// let b=20;
+// let c = a -b;
+// console.log(`the value of c is ${c}`);
+
+let today = new Date();
 
