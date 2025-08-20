5184d34a56777d2df4bae4aebd119caa19d02f60
SHASHI BHOOSHAN RAJ <132089716+Shashi7667@users.noreply.github.com>
2025-08-20 23:44:44 +0530

    Create 03_conversion.js

---
+++ hitesh_js/03_conversion.js
@@ -0,0 +1,17 @@
+let value=4
+
+let negValue = -value
+// console.log(negValue);
+
+console.log(23-23);
+
+let str1 = "shashi"
+let str2 = "raj"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(5 + "4");
+console.log("1" +2 + 2);
+console.log(1 + 2 + "3");
