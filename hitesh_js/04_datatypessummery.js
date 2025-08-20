fee69e40740ba2bd1d6d1a238a882699de1ae1fe
SHASHI BHOOSHAN RAJ <132089716+Shashi7667@users.noreply.github.com>
2025-08-21 00:11:22 +0530

    datatypes summery

---
+++ hitesh_js/04_datatypessummery.js
@@ -0,0 +1,31 @@
+// primitive data types 
+
+// 7 types : String, boolean, number, bigint, symbol, null, undefine
+
+// javascript is a dynamic type language
+
+const score = 100
+const scoreValue =20.3
+
+const isLoggedIn = false
+
+const outsideTemp = null
+let userEmail;  // undefine 
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// Reference data types , non primitive data types
+
+// Arrays, object, function, 
+
+let movie = ["KGF", "Bahubali"];
+console.log(movie);
+
+let user ={
+    name : Shashi,
+    lastName : Raj
+}
+
