1d19f09c2d2b3f2496d5c116f5609976513b9734
SHASHI BHOOSHAN RAJ <132089716+Shashi7667@users.noreply.github.com>
2025-08-25 23:54:36 +0530

    Create 01string.js

---
+++ program-files/01string.js
@@ -0,0 +1,11 @@
+let name = "shashi"
+let repoconst = "Raj"
+
+console.log(name + repoconst);
+
+console.log(`My name is ${name} and my repoconst is ${repoconst}`);
+
+let myName = new String ('Shashi')
+
+console.log(myName);
+console.log(myName[4]);
