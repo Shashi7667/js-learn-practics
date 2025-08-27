78cde08edab3b882ba4a4e912f62877668df80e1
SHASHI BHOOSHAN RAJ <132089716+Shashi7667@users.noreply.github.com>
2025-08-27 18:49:45 +0530

    Create function_w3.js

---
+++ js_intro/function_w3.js
@@ -0,0 +1,43 @@
+function number( a, b){
+    return a * b;
+}
+
+let result = number(10, 30);
+console.log(result);
+
+
+function car(...namee){
+    return namee
+}
+
+let carName = car("Ford", "tata", "alto");
+console.log(carName);
+
+// function car(...names) {
+//     return names;
+// }
+
+// let carName = car("Ford", "tata", "alto");
+// console.log(carName); 
+
+function toCelcius(f){
+    return (5/9) * (f - 32);
+}
+
+let value = toCelcius(32);
+console.log(value);
+
+// arrow function
+// before arrow
+
+let numberss = function(b , c) {return b + c}
+
+let results = numberss(23 , 23);
+console.log(results);
+
+// after arrow
+
+add = (x , y) => x + y;
+
+let addTwoNumber = add(20 , 21);
+console.log(addTwoNumber);
